06784a462ece4a75557ea175dcb7d6d446a99ad1
GingeraleX <81615778+GingeraleX@users.noreply.github.com>
2025-04-15 16:12:12 +0200

    chore: initial commit

---
+++ Requirements/install_requirements.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Installing required Python libraries...
+python -m pip install numpy librosa matplotlib soundfile scipy
+echo Installation complete.
+pause
---
+++ Requirements/install_requirements.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Installing required Python libraries..."
+pip install numpy librosa matplotlib soundfile scipy
+echo "Installation complete."
